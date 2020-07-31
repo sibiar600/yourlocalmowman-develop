42d4b5e1ef53c5bd34eb8117df8e7a2606f225e6
James Pascual <pascualjames@gmail.com>
2020-07-31 02:43:36 -0700

    Added trailing slash to path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/yourlocalmowman-develop",
+  pathPrefix: "/yourlocalmowman-develop/",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
